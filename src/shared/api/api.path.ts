4fee4c726d7b8d93ceaec3d824c438ca5ddef66d
서지유 <jihoseo2008@gmail.com>
2026-03-06 21:26:10 +0900

    remove :: auth 관련 코드 필요없으시잖아 한잔해

--- src/shared/api/api.path.ts
+++
@@ -1,8 +0,0 @@
-export const API_PATH = {
-    AUTH: {
-        SIGNUP: "/auth/signup",
-        LOGIN: "/auth/login",
-        LOGOUT: "/auth/logout",
-        REFRESH: "/auth/refresh",
-    },
-}
--- src/shared/query/query-client.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClient } from "@tanstack/react-query"
-/**
- * @description Tanstack Query Client
- */
-export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {},
-    },
-})

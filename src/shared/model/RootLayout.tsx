802e5b80a2af4f7b030368ccd211efed4908af4d
서지유 <jihoseo2008@gmail.com>
2026-03-12 09:47:15 +0900

    feat :: 페이지 이동 시 스크롤 맨 위로

--- src/shared/model/RootLayout.tsx
+++ src/shared/model/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router"
 import { Header } from "../ui/Header"
+import { ScrollToTop } from "../ui/ScrollToTop"
 
 /**
  * @description 모든 페이지에서 공통으로 사용하는 레이아웃
@@ -7,6 +8,7 @@ import { Header } from "../ui/Header"
 export function RootLayout() {
     return (
         <div className="min-h-screen bg-white dark:bg-slate-950 transition-colors">
+            <ScrollToTop />
             <Header />
 
             <main>

9361df9d869a89bb48bcb71cc800947def60b92d
서지유 <jihoseo2008@gmail.com>
2026-03-19 09:29:12 +0900

    feat :: 페이지 라우터 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,9 @@
+import BrowserPage from "@/pages/browser/BrowserPage"
+import CSPage from "@/pages/cs/CsPage"
 import HtmlCssPage from "@/pages/html-css/HtmlCssPage"
+import JavascriptPage from "@/pages/javascript/JavascriptPage"
 import LandingPage from "@/pages/landing/LandingPage"
+import ReactPage from "@/pages/react/ReactPage"
 import { RootLayout } from "@/shared/model/RootLayout"
 
 import { createBrowserRouter } from "react-router"
@@ -16,6 +20,22 @@ export const router = createBrowserRouter(
                 {
                     path: "/html-css",
                     element: <HtmlCssPage />,
+                },
+                {
+                    path: "/javascript",
+                    element: <JavascriptPage />,
+                },
+                {
+                    path: "/react",
+                    element: <ReactPage />,
+                },
+                {
+                    path: "/browser",
+                    element: <BrowserPage />,
+                },
+                {
+                    path: "/cs",
+                    element: <CSPage />,
                 },
             ],
         },

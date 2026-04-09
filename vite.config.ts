06b17d7611e8422de4128055d821804b7b06abf9
서지유 <jihoseo2008@gmail.com>
2026-04-09 10:13:54 +0900

    faet :: setting pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,50 @@
 import { defineConfig } from "vite"
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            disable: false,
+            registerType: "autoUpdate",
+
+            devOptions: {
+                enabled: true,
+            },
+
+            includeAssets: [
+                "icons/favicon.ico",
+                "icons/icon-192.png",
+                "icons/icon-512.png",
+                "splash/apple-touch-icon.png",
+            ],
+
+            manifest: {
+                name: "FEndbook",
+                short_name: "FEndbook",
+                start_url: "/",
+                theme_color: "#000000",
+                background_color: "#ffffff",
+                display: "standalone",
+                icons: [
+                    {
+                        src: "icons/icon-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icons/icon-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
 
     resolve: {
         alias: {
